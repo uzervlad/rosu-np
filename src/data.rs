025a0cce5263a7052bfc9c73678f4d1ad936169f
OctopuSSX <vladik0701@gmail.com>
2024-06-07 16:10:00 +0300

    Rework GameData

--- src/data.rs
+++ src/data.rs
@@ -1,58 +1,84 @@
 use serde_derive::Deserialize;
 
-#[derive(Default, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GameData {
-  mods: Option<String>,
-  artist_roman: Option<String>,
-  title_roman: Option<String>,
-  diff_name: Option<String>,
-  creator: Option<String>,
-  skin: Option<String>,
-  mapid: Option<u32>,
+macro_rules! generate_structs {
+  ($struct_name:ident, $partial_name:ident, $($name:ident: $type:ty [$token:literal],)+) => {
+    #[derive(Debug, Default)]
+    pub struct $struct_name {
+      $($name: $type,)*
+    }
+
+    impl $struct_name {
+      pub fn update(&mut self, new_data: $partial_name) {
+        $(
+          if let Some($name) = new_data.$name {
+            self.$name = $name;
+          }
+        )*
+      }
+
+      pub fn get_keys() -> Vec<&'static str> {
+        vec![$($token,)*]
+      }
+    }
+
+    #[derive(Deserialize)]
+    pub struct $partial_name {
+      $(
+        #[serde(rename = $token)]
+        $name: Option<$type>,
+      )*
+    }
+  };
+}
+
+generate_structs! {
+  GameData, PartialGameData,
+
+  artist: String ["artistRoman"],
+  title: String ["titleRoman"],
+  version: String ["diffName"],
+  creator: String ["creator"],
+  mods: String ["mods"],
+  skin: String ["skin"],
+  map_id: u32 ["mapid"],
+
+  pp_98: f32 ["osu_98PP"],
+  pp_99: f32 ["osu_99PP"],
+  pp_ss: f32 ["osu_SSPP"],
+
+  pp_mods_98: f32 ["osu_m98PP"],
+  pp_mods_99: f32 ["osu_m99PP"],
+  pp_mods_ss: f32 ["osu_mSSPP"],
+
+  gamemode: String ["gameMode"],
+
+  pp_mania: f32 ["mania_1_000_000PP"],
+  pp_mods_mania: f32 ["mania_m1_000_000PP"],
 }
 
 impl GameData {
-  pub fn update(&mut self, new_data: GameData) {
-    if let Some(mods) = new_data.mods {
-      self.mods = Some(mods);
-    }
-    if let Some(artist_roman) = new_data.artist_roman {
-      self.artist_roman = Some(artist_roman);
-    }
-    if let Some(title_roman) = new_data.title_roman {
-      self.title_roman = Some(title_roman);
-    }
-    if let Some(diff_name) = new_data.diff_name {
-      self.diff_name = Some(diff_name);
-    }
-    if let Some(creator) = new_data.creator {
-      self.creator = Some(creator);
-    }
-    if let Some(skin) = new_data.skin {
-      self.skin = Some(skin);
-    }
-    if let Some(mapid) = new_data.mapid {
-      self.mapid = Some(mapid);
-    }
+  fn get_mods(&self) -> String {
+    self.mods.split(',').collect::<String>()
   }
 
   pub fn get_beatmap_string(&self) -> String {
-    match (&self.artist_roman, &self.title_roman, &self.diff_name, &self.creator, self.mapid) {
-      (Some(artist_roman), Some(title_roman), Some(diff_name), Some(creator), Some(mapid)) => 
-        if mapid != 0 {
-          format!("{} - {} [{}] by {} https://osu.ppy.sh/b/{}", artist_roman, title_roman, diff_name, creator, mapid)
-        } else {
-          format!("{} - {} [{}] by {}", artist_roman, title_roman, diff_name, creator)
-        },
-      _ => String::new(),
+    if self.map_id != 0 {
+      format!("{} - {} [{}] by {} https://osu.ppy.sh/b/{}", self.artist, self.title, self.version, self.creator, self.map_id)
+    } else {
+      format!("{} - {} [{}] by {}", self.artist, self.title, self.version, self.creator)
+    }
+  }
+
+  pub fn get_pp_string(&self) -> String {
+    match (self.gamemode.as_str(), self.pp_mania == self.pp_mods_mania, self.pp_ss == self.pp_mods_ss) {
+      ("OsuMania", true, _) => format!("PP (1m): {:.1}", self.pp_mania),
+      ("OsuMania", false, _) => format!("PP (1m): {:.1} +{} {:.1}", self.pp_mania, self.get_mods(), self.pp_mods_mania),
+      (_, _, true) => format!("PP (98/99/100): {:.1}/{:.1}/{:.1}", self.pp_98, self.pp_99, self.pp_ss),
+      (_, _, false) => format!("PP (98/99/100): {:.1}/{:.1}/{:.1} +{} {:.1}/{:.1}/{:.1}", self.pp_98, self.pp_99, self.pp_ss, self.get_mods(), self.pp_mods_98, self.pp_mods_99, self.pp_mods_ss),
     }
   }
 
   pub fn get_skin(&self) -> String {
-    match &self.skin {
-      Some(skin) => skin.clone(),
-      None => String::new(),
-    }
+    self.skin.clone()
   }
 }
--- src/sc_thread.rs
+++ src/sc_thread.rs
@@ -4,21 +4,14 @@ use futures_util::{SinkExt, StreamExt};
 use tokio::{sync::Mutex, time::sleep};
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 
-use crate::data::GameData;
-
-const INIT_MESSAGE: &str = r#"[
-  "artistRoman",
-  "titleRoman",
-  "diffName",
-  "creator",
-  "mapid",
-  "skin"
-]"#;
+use crate::data::{GameData, PartialGameData};
 
 pub async fn sc_thread(game_data: Arc<Mutex<GameData>>) {
   let restart_timeout = Duration::from_secs(2);
 
   let url = "ws://localhost:20727/tokens";
+
+  let init_message = serde_json::to_string(&GameData::get_keys()).unwrap();
 
   loop {
     let ws_stream = match connect_async(url).await {
@@ -33,7 +26,7 @@ pub async fn sc_thread(game_data: Arc<Mutex<GameData>>) {
 
     let (mut ws_write, ws_read) = ws_stream.split();
 
-    if let Err(e) = ws_write.send(Message::Text(INIT_MESSAGE.to_owned())).await {
+    if let Err(e) = ws_write.send(Message::Text(init_message.clone())).await {
       println!("Failed to initialize StreamCompanion tokens: {}", e);
       println!("Reconnecting...");
       sleep(restart_timeout).await;
@@ -45,7 +38,7 @@ pub async fn sc_thread(game_data: Arc<Mutex<GameData>>) {
     let ws_game_data = game_data.clone();
     ws_read.for_each(|message| async {
       if let Ok(Message::Text(data)) = message {
-        let new_data = serde_json::from_str::<GameData>(&data).unwrap();
+        let new_data = serde_json::from_str::<PartialGameData>(&data).unwrap();
         let mut game_data = ws_game_data.lock().await;
         game_data.update(new_data);
       }
